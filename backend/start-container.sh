d767ec70a6265d851d70eae9d791665af64b996a
Yassine Mabrouk <yassinemab04@gmail.com>
2022-10-21 01:36:18 +0200

    added react app with ts, added django project but connection to database fails, added docker containers for frontend, backend and database

---
+++ backend/start-container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Make database migrations"
+python manage.py makemigrations
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+echo "Run server"
+python manage.py runserver 0.0.0.0:8000
